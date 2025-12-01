9078555853aad8af3fbcb541e7c651390a8be5de
AlanRanchs <alanjr.isc@gmail.com>
2025-11-30 22:38:33 -0600

    Se Implementa la funcionalidad del carrito con operaciones de añadir, eliminar y recuperar; actualizar la visualización de los alimentos para utilizar los ID de los artículos de forma coherente.

---
+++ backend/controllers/cartController.js
@@ -0,0 +1,54 @@
+import userModel from '../models/userModel.js';
+
+// Add to Cart
+const addToCart = async (req, res) => {
+    try {
+        let userData = await userModel.findById(req.body.userId);
+        let cartData = userData.cartData || {};
+        
+        if (!cartData[req.body.itemId]) {
+            cartData[req.body.itemId] = 1;
+        }
+        else {
+            cartData[req.body.itemId] += 1;
+        }
+        
+        await userModel.findByIdAndUpdate(req.body.userId, {cartData});
+        res.json({success:true, message: "Comida agregada al carrito"});
+    } catch (error) {
+        console.log(error);
+        res.json({success:false, message: "Error al agregar comida al carrito"});
+    }
+}
+
+// Remove from cart
+const removeFromCart = async (req, res) => {
+    try {
+        let userData = await userModel.findById(req.body.userId);
+        let cartData = userData.cartData || {};
+        
+        if (cartData[req.body.itemId] > 0){
+            cartData[req.body.itemId] -= 1;
+        }
+        
+        await userModel.findByIdAndUpdate(req.body.userId, {cartData});
+        res.json({success:true, message: "Comida eliminada del carrito"});
+    } catch (error) {
+        console.log(error);
+        res.json({success:false, message: "Error al eliminar comida del carrito"});
+    }
+}
+
+// fetch user cart data
+const getCart = async (req, res) => {
+    try {
+        let userData = await userModel.findById(req.body.userId);
+        let cartData = userData.cartData || {};
+        res.json({success:true, cartData});
+    } catch (error) {
+        console.log(error);
+        res.json({success:false, message: "Error al obtener datos del carrito"});
+    }
+}
+
+export { addToCart, removeFromCart, getCart };
--- frontend/src/components/FoodDisplay/FoodDisplay.jsx
+++ frontend/src/components/FoodDisplay/FoodDisplay.jsx
@@ -12,7 +12,7 @@ export const FoodDisplay = ({category}) => {
         <div className='food-display-list'>
             {food_list.map((item,index)=>{
               if(category==="All" || category===item.category){
-                return <FoodItem key={index} id={item.id} name={item.name} price={item.price} description={item.description} image={item.image}/>
+                return <FoodItem key={index} id={item._id} name={item.name} price={item.price} description={item.description} image={item.image}/>
               }
             })}
         </div>
